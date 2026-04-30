c48b2f088218544403e533438134d053d03774f9
Anderson Prado <136194018+andersonprado1@users.noreply.github.com>
2026-04-30 08:20:07 -0300

    Resolvendo o algoritmo de matemática

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,6 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+num1 = input('Insira o primeiro número')
+num2 = input('Insira o segundo número')
+
+soma = num1 + num2
+print(f'a soma dos números {num1} e {num2} é: {soma}')
