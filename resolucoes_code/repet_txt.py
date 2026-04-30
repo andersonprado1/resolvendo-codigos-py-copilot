a7a17ceb7542e021341276358f20911ac47cbfa9
Anderson Prado <136194018+andersonprado1@users.noreply.github.com>
2026-04-30 08:26:07 -0300

    terceiro algoritmo

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,5 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+string = input("insira um texto: ")
+num = int(input('Digite o número de repetições do textro: '))
+
+print(string*num)
